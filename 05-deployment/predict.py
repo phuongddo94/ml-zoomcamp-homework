3e5f22347355d2625743b4d6a592542c8f4057cd
Phuong Do <47506179+phuongddo94@users.noreply.github.com>
2025-08-19 10:04:36 +0000

    model prediction using web service

--- 05-deployment/predict.py
+++ 05-deployment/predict.py
@@ -1,6 +1,26 @@
 import pickle
+import os
 
-input_file = 'model_C=1.0.bin'
+from flask import Flask, request, jsonify
 
-with open(input_file, 'rb') as f_in:
+model_file = os.path.join(os.getcwd(), '05-deployment', 'model_C=1.0.bin')
+
+with open(model_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
+
+app = Flask('churn')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[:, 1]
+    churn = y_pred >= 0.5
+
+    return jsonify({
+        'churn_probability': float(y_pred),
+        'churn': bool(churn)
+        })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9696)
